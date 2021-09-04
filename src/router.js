2971a318f82fe62059da8c981f574eb98b30226b
Black Nguyen <blackng10.5.1997@gmail.com>
2021-09-04 12:20:50 +0700

    update

--- src/router.js
+++ src/router.js
@@ -15,6 +15,7 @@ const router = createRouter({
     {
       path: '/coaches/:id',
       component: CoacheDetail,
+      props: true,
       children: [{ path: 'contact', component: ContactCoach }] //coaches/1/contact
     },
     { path: '/register', component: CoachRegistration },
