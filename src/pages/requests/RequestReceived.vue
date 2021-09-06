9faec483b19e4c331fab40b419f7b2607a1b4d04
Black Nguyen <blackng10.5.1997@gmail.com>
2021-09-06 13:19:10 +0700

    update

--- src/pages/requests/RequestReceived.vue
+++ src/pages/requests/RequestReceived.vue
@@ -4,14 +4,25 @@
       <header>
         <h2>Requests Received</h2>
       </header>
-      <ul v-if="hasRequests"></ul>
+      <ul v-if="hasRequests">
+        <request-item
+          v-for="req in receivedRequests"
+          :key="req.id"
+          :email="req.userEmail"
+          :message="req.message"
+        ></request-item>
+      </ul>
       <h3 v-else>You havent't received any requests yet</h3>
     </base-card>
   </section>
 </template>
 
 <script>
+import RequestItem from '../../components/requests/RequestItem.vue';
 export default {
+  components: {
+    RequestItem
+  },
   computed: {
     receivedRequests() {
       return this.$store.getters['requests/requests'];
